197dbbe850a9c9e03d80e4046a004735d669667f
KeyC0de <nik.lazkey@gmail.com>
2021-09-10 14:19:54 +0300

    cleaned

--- crtp/main.cpp
+++ crtp/main.cpp
@@ -1,27 +1,52 @@
 #include <iostream>
+#if defined _DEBUG && !defined NDEBUG
+#	include <vld.h>
+#endif
 
-
-static int nodes = 0;
 
 class TreeNode
 {
-private:
+	static inline int m_nodes = 0;
 	int m_id;
+	TreeNode* m_pLeft;
+	TreeNode* m_pRight;
 public:
-	TreeNode* left;
-	TreeNode* right;
-
 	TreeNode()
-		: m_id(++nodes),
-		left(nullptr),
-		right(nullptr)
-	{}
-	TreeNode( int id )
-		: m_id(id),
-		left(nullptr),
-		right(nullptr)
+		:
+		m_id(++m_nodes),
+		m_pLeft{nullptr},
+		m_pRight{nullptr}
 	{
-		++nodes;
+
+	}
+
+	TreeNode( int id )
+		:
+		m_id(id),
+		m_pLeft{nullptr},
+		m_pRight{nullptr}
+	{
+		++m_nodes;
+	}
+
+	void setLeft( TreeNode* left )
+	{
+		m_pLeft = left;
+	}
+
+	void setRight( TreeNode* right )
+	{
+		m_pRight = right;
+	}
+
+	TreeNode* getLeft()
+	{
+		return m_pLeft;
+	}
+
+	TreeNode* getRight()
+	{
+		return m_pRight;
 	}
 
 	int getId() const
@@ -34,6 +59,7 @@ public:
 template<typename T>
 class TreeParser
 {
+	static inline uint32_t parsers = 0;
 protected:
 	//template<typename T, typename = std::enable_if_t<std::is_base_of_v<TreeParser, T>>>
 	TreeParser()
@@ -41,14 +67,13 @@ protected:
 		++parsers;
 	}
 public:
-	static uint32_t parsers;
 	void preorderTraversal( TreeNode* node )
 	{
 		if ( node != nullptr )
 		{
 			processNode( node );
-			preorderTraversal( node->left );
-			preorderTraversal( node->right );
+			preorderTraversal( node->getLeft() );
+			preorderTraversal( node->getRight() );
 		}
 	}
 
@@ -57,25 +82,27 @@ public:
 	/// \brief the 'interface' method
 	void processNode( TreeNode* node )	// 2, 3. the generic algorithm is customized by derived classes
 	{
-		static_cast<T*>( this )->processNode( node );	// depending on the client's demand - the right function will be called
+		static_cast<T*>( this )->processNode( node );	// depending on the client's demand - the m_pRight function will be called
 	}
 };
-
-template<class T>
-uint32_t TreeParser<T>::parsers = 0;
 
 class SpecializedTreeParser1
 	: public TreeParser<SpecializedTreeParser1> // 1. is-a relationship
 {
 public:
 	explicit SpecializedTreeParser1()
-		: TreeParser()
-	{}
+		:
+		TreeParser()
+	{
+
+	}
 
 	void processNode( TreeNode* node )
 	{
 		std::cout << "Customized (derived - SpecializedTreeParser1) preorderTraversal() - "
-			"id=" << node->getId() << '\n';
+			"id="
+			<< node->getId()
+			<< '\n';
 	}
 };
 
@@ -84,37 +111,50 @@ class SpecializedTreeParser2
 {
 public:
 	explicit SpecializedTreeParser2()
-		: TreeParser()
-	{}
+		:
+		TreeParser()
+	{
+
+	}
 
 	void processNode( TreeNode* node )
 	{
 		std::cout << "Customized (derived - SpecializedTreeParser2) preorderTraversal() - "
-			"id=" << node->getId() << '\n';
+			"id="
+			<< node->getId()
+			<< '\n';
 	}
 };
 
 
-int main() 
+int main()
 {
 	TreeNode root;
 	TreeNode leftChild;
 	TreeNode rightChild;
 
-	root.left = &leftChild;
-	root.right = &rightChild;
+	root.setLeft( &leftChild );
+	root.setRight( &rightChild );
 
-	std::cout << "Root id: " << root.getId() << '\n';
-	std::cout << "Left child id: " << leftChild.getId() << '\n';
-	std::cout << "Right child id: " << rightChild.getId() << '\n';
+	std::cout << "Root id: "
+		<< root.getId()
+		<< '\n';
+	std::cout << "Left child id: "
+		<< leftChild.getId()
+		<< '\n';
+	std::cout << "Right child id: "
+		<< rightChild.getId()
+		<< '\n';
 
-	SpecializedTreeParser1 _1;
-	_1.preorderTraversal( &root );
+	SpecializedTreeParser1 tp1;
+	tp1.preorderTraversal( &root );
 
-	SpecializedTreeParser2 _2;
-	_2.preorderTraversal( &root );
+	SpecializedTreeParser2 tp2;
+	tp2.preorderTraversal( &root );
 	// only 1 of 2 types of parsing may happen at a program's build though
-	//std::cout << "#parsers=" << _2.parsers << '\n';
+	//std::cout << "#parsers="
+	//	<< tp2.parsers
+	//	<< '\n';
 
 	std::system( "pause" );
 	return EXIT_SUCCESS;
